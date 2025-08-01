dbc1baa2223661d90dfd2a556a47afd3a387eb0b
Sandro Pereira Dev <smpsandro1239@gmail.com>
2025-08-01 08:54:51 +0100

    criar rota simple para detetar erro 	modified:   routes/simple.php

--- routes/simple.php
+++ routes/simple.php
@@ -9,3 +9,10 @@ Route::get('/simple', function () {
 Route::get('/test-web', function () {
   return 'TESTE COM WEB MIDDLEWARE!';
 })->middleware('web');
+Route::get('/test-encrypt', function () {
+  return 'TESTE ENCRYPT COOKIES!';
+})->middleware(\App\Http\Middleware\EncryptCookies::class);
+
+Route::get('/test-session', function () {
+  return 'TESTE SESSION!';
+})->middleware(\Illuminate\Session\Middleware\StartSession::class);
